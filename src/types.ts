0e938d4c7fa6ab399fe9f5389cce072e10730cce
Victor Lima <victor.lms00@gmail.com>
2025-11-11 17:50:33 -0300

    adding main interfaces

---
+++ src/types.ts
@@ -0,0 +1,33 @@
+export interface IService {
+	id: string;
+	name: string;
+	duration: number;
+	price: number;
+	description: string;
+}
+
+export interface ICenterInfo {
+	slug: string;
+	name: string;
+	logoUrl: string;
+	description: string;
+}
+
+export interface IBookingData {
+	serviceId: string;
+	serviceName: string;
+	clientName: string;
+	clientEmail: string;
+	date: string;
+	time: string;
+}
+
+export interface ICenterData extends ICenterInfo {
+	services: IService[];
+}
+
+export interface CenterPageProps {
+	params: {
+		center: string;
+	};
+}
